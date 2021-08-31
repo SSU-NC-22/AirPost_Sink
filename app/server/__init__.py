11b8f21f45d98171296e7b067b2f5cefccf4a36b
jongsoo <jsoone24@gmail.com>
2021-08-31 13:39:44 +0900

    time stamp format update

---
+++ app/server/__init__.py
@@ -0,0 +1,206 @@
+import ast
+import datetime
+import json
+import time
+import threading
+import socket
+
+import paho.mqtt.client as mqtt
+from flask import Flask
+from flask import request
+from kafka import KafkaProducer
+from message.mqtt_message import MqttMessages
+
+from .healthcheck import HealthCheck
+from .actuator import Actuator
+from .http_codes import http_response_code
+from .setup import args
+
+
+def on_connect(client, userdata, flags, rc):
+    print("connected to mqtt broker")
+
+
+def on_subscribe():
+    print('subscribed')
+
+
+def on_message(client, userdata, message):
+    print('messaging')
+
+# 할 일 : 메시지 받아서 카프카로 전송하는 메시지 형식 변경 필요 mqtt_message.py 파일 수정.
+# give message to kafka as kafka producer
+def send_message_to_kafka(msg):
+    ### Look ###
+    v_topic = msg.topic.split('/')  # 토픽이 data/# 이런식으로 오면 쪼개서 [data, #]으로 저장
+    # payload = msg.payload.decode().split(',')  # 싱크노드에서 오는 데이터형식은 일렬로 #, #, #, ... 으로 온다. 컴마로 쪼개서 리스트로 저장.
+    # kafka_message = topic_manager.kafka_message(v_topic, payload)  # 첫번째 인자로 센서의 id 전송. payload는 리스트로 저장된 센서값들
+    kafka_message = msg.payload.decode() # dict 형식으로 변환
+    topic_manager.add_node(int(v_topic[1])) # 카프카로 보낼때 센서 노드 추가
+    # topic_manager.add_sensor(int(v_topic[1]), int(payload[0])) # 노드 id, 센서 id 넣어서 일치 시 센서 추가
+    # if topic_manager.sensor_check(v_topic[1], payload):
+    if len(topic_manager.get_nodes()) > 0:
+        '''
+        if health_check.get_health_check_mode():
+            if(health_check.set_node_state(v_topic[1], True)):
+                print("health check: ", v_topic[1], "->True")
+            else:
+                print("This node is not healthcheck target: ",v_topic[1])
+        '''
+        print("data by mqtt: sending message to kafka : %s" % msg)
+        print(kafka_message)
+        
+        producer.send("sensor-data", kafka_message)
+        producer.flush()
+
+
+def handle_uplink_command(msg):
+    v_topic = msg.topic.split('/')  # command / uplink / MacCommand / nodeid
+    if v_topic[2] == 'DevStatusAns':
+        print('Received DevStatusAns!')
+        json_msg = json.loads(str(msg.payload.decode()))
+        health_check.set_node_state(v_topic[3], True, json_msg['battery'])
+
+# callbacks
+
+
+def data_callback(client, userdata, msg):
+    return send_message_to_kafka(msg)
+
+
+def command_callback(client, userdata, msg):
+    return handle_uplink_command(msg)
+
+# connecting mqtt client to mqtt broker
+
+
+def mqtt_run():
+    client.on_connect = on_connect
+    #client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.connect(args.b, 1883)
+    client.loop_start()
+    client.message_callback_add("data/#", data_callback)
+    client.message_callback_add("command/uplink/#", command_callback)
+    client.subscribe("data/#")
+    client.subscribe("command/uplink/#")
+
+    return http_response_code['success200']
+
+
+def on_disconnect(client, user_data, rc):
+    print("Disconnected")
+    client.disconnect()
+
+
+def health_check_handler():
+    while(1):
+        if health_check.get_health_check_mode():
+            healthcheck_server = '192.168.0.18'  # '220.70.2.5'
+            healthcheck_port = 8085
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print('Connect to HealthCheck Server...')
+            client_socket.connect((healthcheck_server, healthcheck_port))
+            print("Connected to HealthCheck...")
+
+            print("healthcheck target: ", topic_manager.get_nodes())
+
+            ### Look ###
+            health_check.setup_target_nodelist(topic_manager.get_nodes())
+            health_check.send_req(client)
+            time.sleep(health_check.get_time())
+            print("health_check: Send Json to HealthCheck Server...")
+            client_socket.sendall(health_check.create_msg())
+            ###      ###
+
+# start the node webserver
+
+
+app = Flask(__name__)
+producer = KafkaProducer(bootstrap_servers=[args.k+':9092'], api_version=(0, 10, 2, 0), value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+topic_manager = MqttMessages()
+client = mqtt.Client()
+app.debug = False
+#app.threaded = True
+health_check = HealthCheck()
+actuator = Actuator()
+mqtt_run()
+# create socket and run health_check thread
+health_check.set_health_check_mode(True)
+th = threading.Thread(target=health_check_handler, args=())
+th.start()
+
+
+# setting interval of the health check time
+@app.route('/health-check/set_time/<time>', methods=['GET'])
+def health_check_set_time():
+    health_check.set_time(time)
+    return http_response_code['success200']
+
+
+# interval of the health check time
+@app.route('/health-check/time', methods=['GET'])
+def health_check_get_time():
+    health_check.get_time()
+    return http_response_code['success200']
+
+
+# make the format of the topics from the data which toiot server gave
+@app.route('/topics', methods=['POST'])
+def response_getMessageFormat():
+    topic_manager.clear_topics()
+    temp = json.loads(request.get_data().decode())
+    topic_manager.get_message_format(temp)
+    client.subscribe(topic_manager.mqtt_topic)
+    print(topic_manager.mqtt_topic)
+    return http_response_code['success200']
+
+
+# delete sensor
+@app.route('/sensor/<node>/<sensor>', methods=['GET', 'DELETE'])
+def delete_sensor(sensor):
+    client.unsubscribe(topic_manager.get_delete_sensor(sensor))
+    return http_response_code['success200']
+
+
+# delete arduino board
+@app.route('/node/<node>', methods=['GET', 'DELETE'])
+def delete_node(node):
+    client.unsubscribe(topic_manager.get_delete_node(node))
+    return http_response_code['success200']
+
+# handle actuator
+
+
+@app.route('/actuator', methods=['GET', 'POST'])
+def actuator_command():
+    json_data = request.get_json(silent=True)
+    actuator.send_req(client, json_data)
+    return http_response_code['success200']
+
+# error handlers
+
+
+@app.errorhandler(400)
+def page_bad_request(error):
+    return http_response_code['error400']
+
+
+@app.errorhandler(401)
+def page_unauthorized(error):
+    return http_response_code['error401']
+
+
+@app.errorhandler(403)
+def page_forbidden(error):
+    return http_response_code['error403']
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return http_response_code['error404']
+
+
+@app.errorhandler(408)
+def page_timeout(error):
+    return http_response_code['error408']
---
+++ test/station.py
@@ -0,0 +1,84 @@
+from paho.mqtt import client as mqtt_client
+import datetime
+import time
+import json
+
+class MQTT():
+	def __init__(self, broker, port, client_id):
+		self.broker = broker
+		self.port = port
+		self.client_id = client_id
+		self.client = mqtt_client.Client(self.client_id)
+	
+	def connect_mqtt(self):
+		def on_connect(client, userdata, flags, rc):
+			if rc == 0:
+				print("Connected to MQTT Broker!")
+			else:
+				print("Failed to connect, return code %d\n", rc)
+
+		self.client.on_connect = on_connect
+		self.client.connect(self.broker, self.port)
+		self.client.loop_start()
+
+	def publish(self, topic, msg): #input msg type : string
+		self.pub_topic = topic
+		result = self.client.publish(self.pub_topic, msg)
+
+		# result: [0, 1]
+		status = result[0]
+		if status == 0:
+			print(f"Send msg to topic `{self.pub_topic}`")
+		else:
+			print(f"Failed to send message to topic {self.pub_topic}")
+
+
+	def subscribe(self, topic, handler):
+		self.sub_topic = topic
+		self.handler = handler
+		
+		def on_message(client, userdata, msg): #return msg.payload
+			print(f"Received msg from `{msg.topic}` topic")
+			self.handler.run(msg.payload)
+
+		self.client.on_message = on_message
+		self.client.subscribe(self.sub_topic)
+
+def Handler():
+	return 
+
+broker = '58.230.119.87'
+port = 9708
+client_id = '0'
+
+handler = Handler()
+mqtt = MQTT(broker, port, client_id)
+mqtt.connect_mqtt()
+mqtt.subscribe("data/0", handler)
+#mqtt.subscribe("command/downlink/ActuatorReq/0", handler)
+
+
+while True:	
+	msgs = {
+		"node_id": client_id,
+		"values": {
+			"temp": None,
+			"humid": None,
+			"light": None,
+
+			"lat": None,
+			"lon": None,
+			"alt": None,
+
+			"clear": None,
+			"status": 0,
+		},
+		"timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	}
+	
+	msg = json.dumps(msgs)
+
+	mqtt.publish("data/0", msg)
+	print("publish: ", type(msg), msg, "\n")
+
+	time.sleep(1)
